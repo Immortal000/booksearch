919053f4f7e397008a568bd0e356ff84debec149
Siddarth Kotthur <54898201+Immortal000@users.noreply.github.com>
2019-10-20 22:44:50 -0500

    Update pricing.py

--- pricing.py
+++ pricing.py
@@ -5,10 +5,14 @@ book_title = input()
 website = requests.get('https://www.bookdepository.com/search?searchTerm='+book_title).text
 soup = BeautifulSoup(website,'lxml')
 b = soup.find('div',{'class':'price-wrap'}).text.strip().split()
+book_repository = b[0]
 
 website = requests.get('https://www.abebooks.com/servlet/SearchResults?cm_sp=SearchFwi-_-SRP-_-Results&isbn=0439023513&kn='+book_title).text
 soup = BeautifulSoup(website,'lxml')
-price = soup.find('div',{'class':'srp-item-price'}).text.strip()
+try:
+  abe_books = soup.find('div',{'class':'srp-item-price'}).text.strip()
+except: 
+  abe_books = soup.find('span',{'class':'price-muted'}).text.strip()
 
-print(b[0],price)
+print(book_depository,abe_books)
 
